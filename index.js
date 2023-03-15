6e819362a7db74bf84dc5fa236ce18ba77c8757c
DanAtibu <sdatibu@asystresources.com>
2023-03-15 07:48:47 -0700

    add ci

--- index.js
+++ index.js
@@ -3,12 +3,18 @@ const text = document.getElementById("text-container");
 
 let array = [
     "Discover conceptually what workflows and GitHub actions are",
-    "Whereas recognition of the inherent dignity"
+    "Whereas recognition of the inherent dignity",
+    "Learn what terms like action, step, job, event, workflow, etc.. mean",
+    "Some lectures will require some basic docker knowledge (it's not necessary if you want to skip it)",
+    "Using GitHub Actions you will find a lot of official and community pre-made workflows and also the actions marketplace.",
+    "In this GitHub Actions Tutorial we go through the following topics",
+    "After that I explain the basic concepts of GitHub Actions, including the GitHub Events and Actions and workflow and HOW GitHub Actions actually automates these workflows using these components.",
+    "I will explain shortly why it is not just another ci/cd tool or what are the benefits of GitHub Actions ci cd pipeline."
 ];
 
 window.addEventListener("load", () => {
-    let init = 0;
-    let sentence_index = 0;
+    var init = 0;
+    var sentence_index = 0;
     setInterval(() => {
         const sentence = array[sentence_index];
         init++;
@@ -17,7 +23,7 @@ window.addEventListener("load", () => {
             init = 0;
             sentence_index < array.length - 1 ? sentence_index++ : sentence_index = 0;
         }
-    }, 100);
+    }, 50);
 });
 
 
